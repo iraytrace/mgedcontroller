a167a1a1e032544b72003edbc3f07660873c5ddb
Lee A. Butler <lee.a.butler6.civ@army.mil>
2026-04-14 17:23:05 -0400

    refactor: convert to use exceptions for errors.

--- MgedSession.py
+++ MgedSession.py
@@ -12,6 +12,10 @@ import time
 import sys
 import shutil
 
+class MgedException(Exception):
+    """Exception raised for talking with mged"""
+    pass
+
 class MgedSession:
     def __init__(self, mged_path):
         self.mged_path = mged_path
@@ -29,6 +33,10 @@ class MgedSession:
     def command_completed(self):
         data = [x.strip() for x in list(self.output.queue)]
         return any(s.startswith(self.mged_prompt) for s in data)
+
+    @property
+    def running(self):
+        return bool(self.process_running and self.thread_running)
 
     @property
     def process_running(self):
@@ -72,57 +80,77 @@ class MgedSession:
                 bufsize=1, # line buffering
                 creationflags=subprocess.CREATE_NO_WINDOW if os.name == 'nt' else 0)
         except Exception as e:
-            print(f"Failed to launch: {e}")
-            return
+            raise MgedException(f"Mged failed to launch: {commandline} : {e}")
 
-        self.thread = threading.Thread(target=self.thread_func,
-                                    args=(self.process.stdout,
-                                            self.output))
-        self.thread.daemon = True
-        self.thread.start()
-
+        try:
+            self.thread = threading.Thread(target=self.thread_func,
+                                        args=(self.process.stdout,
+                                                self.output))
+            self.thread.daemon = True
+            self.thread.start()
+        except Exception as e:
+            raise MgedException("MgedSession failed to start thread")
 
         # wait for process to start responding
+        self.send_command(f'puts "hello"')
 
-        mged.send_command(f'puts "hello"')
+        mged_has_responded = False
+        seconds = 1
+        time.sleep(1) # give subprocess a chance to get started
 
-        good = False
-        seconds = 0
-        while self.process_running and self.thread_running and self.pending_commands > 0:
-            if mged.command_completed:
-                good = True
+        while self.running and self.pending_commands > 0 and seconds < 15:
+            if self.command_completed:
+                mged_has_responded = True
                 break
-            else:
-                print(f"waiting for mged hello {seconds}: No output received. Process running: {self.process_running}, Thread alive: {self.thread_running}")
-                seconds += 1
-                time.sleep(1)
 
-        if not good:
-            print('mged launch failure')
-            sys.exit(1)
+            print(f"Waiting for mged launch {seconds}s")
+            seconds += 1
+            time.sleep(1) # give subprocess a chance to get started
+
+        if not mged_has_responded:
+            raise MgedException('mged launch failure after {seconds} seconds.\n' +
+                                ' Process: {self.process_running}\n' +
+                                ' Thread: {self.thread_running}\n' +
+                                ' Commands Pending: {self.pending_commands}')
 
     def send_command(self, command):
-        if mged.process.poll() is not None:
-            print('process stopped')
-            return
+        if self.process is None:
+            raise MgedException("No mged session {self.process}")
+        if self.process.poll() is not None:
+            raise MgedException("Mged process stopped")
+        if self.thread is None or not self.thread_running:
+            raise MgedException("No read thread")
 
         self.process.stdin.write(command + '\n')
         self.process.stdin.flush()
         self.in_flight += 1
 
     def shutdown(self):
+
         if self.process.poll() is None:
-
-            print("Closing stdin and terminating process...")
+            self.send_command('q')  # attempt shutdown
             self.process.stdin.close()
-            self.process.terminate() # or process.kill()
-            self.process.wait(timeout=2)
 
+            for seconds in range(3): # wait 3 seconds for mged to quit
+                if self.process.poll() is None:
+                    print(f'waiting for mged to quit {seconds}')
+                    time.sleep(1)
+
+            if self.process.poll() is None:
+                # still running
+                print("terminating process...")
+                self.process.terminate() # or process.kill()
+                self.process.wait(timeout=2)
+            else:
+                print(f'mged process done {self.process.poll()}')
         self.thread.join(timeout=2)
 
-
-if __name__ == '__main__':
+def main():
     mged_exe = shutil.which('mged.exe')
+    if not mged_exe or not os.path.exists(mged_exe):
+        print('no mged executable found: {mged_exe}')
+        return
+
     mged = MgedSession(mged_exe)
     mged.launch()
 
@@ -134,8 +162,7 @@ if __name__ == '__main__':
         if mged.command_completed:
             while mged.command_completed:
                 print(f'OUTPUT {i}: {mged.get_output()}')
-        else:
-            # This is expected if there's no output during the 1-second timeout
+        elif mged.pending_commands:
             print(f"Loop {i}: No output received. {mged.pending_commands} pending.\n\tProcess running: {mged.process.poll() is None}, Thread alive: {mged.thread.is_alive()}")
             time.sleep(1)
         # Cleanly close the process
@@ -144,3 +171,7 @@ if __name__ == '__main__':
         print(f'{mged.in_flight} still in flight')
 
     mged.shutdown()
+
+        
+if __name__ == '__main__':
+    main()
